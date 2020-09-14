c7ce1a46605091ed83dddc8f95c5ae440904334a
SouhardyaSaha <souhardyasaha98@gmail.com>
2020-09-15 03:14:00 +0600

    Deploying to heroku

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
-
-app.use(express.static('./dist/courseProjectOne'));
-
+app.use(express.static(__dirname + '/dist/course-project-one'));
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/courseProjectOne' }
-  );
+  res.sendFile(path.join(__dirname +
+    '/dist/course-project-one/index.html'));
 });
-
-
-
 app.listen(process.env.PORT || 8080);
-
-console.log(`Running on port ${process.env.PORT || 8080}`)
